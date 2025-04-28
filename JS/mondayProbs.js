e31365d4d27f3a9eec71a66ca3934567085cd0c8
livingstonkolbie <kolbie.livingston@mscoding.org>
2025-04-28 10:40:30 -0600

    practice probs

---
+++ JS/mondayProbs.js
@@ -0,0 +1,14 @@
+// day 9 combining conditionals and loops
+
+// console.log only EVEN numbers between 1 and 50
+
+for(let i = 1; i <= 50; i++) {
+    if(i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+// counting vowels in a string
+
+let sentence = "JavaScript is awesome!";
+let vowels = "aeiou";
